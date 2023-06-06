7ad90e575d73b4043e710ca5e0f3a97c97893405
Josias Klaus <josias.klaus@web.de>
2023-06-06 16:42:16 +0200

    Reworked mapView to closer resemble concept 🖼️

--- src/views/mapView.jsx
+++ src/views/mapView.jsx
@@ -1,57 +1,63 @@
-import {StyleSheet, View} from 'react-native';
-import {useState} from 'react';
-import MapView, { Marker } from 'react-native-maps';
+import {StyleSheet} from 'react-native';
+import {useState, useEffect} from 'react';
+import MapView, { Circle, Marker } from 'react-native-maps';
+import { TextInput } from 'react-native-paper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet';
+import Slider from '@react-native-community/slider';
 
 import getLocation from '../utils/geolocation';
 import getStationList from '../api/tankerkoenig';
-import { Button } from 'react-native-paper';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-
+import mapStyle from '../utils/mapstyle';
 
 function MapScreen() {
   const [location, setLocation] = useState(false);
   const [radius, setRadius] = useState(1);
   const [stations, setStations] = useState([]);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if(location) {
+      getStationList(location.latitude, location.longitude, radius).then(
+        (data) => {
+          setStations(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
+  }, [location, radius]);
 
   if (!location) {
     getLocation(setLocation);
   }
 
-  const loadStationData = () => {
-    if(location) {
-      console.log("Requesting stations for location: " + location.latitude + ", " + location.longitude);
-      getStationList(location.latitude, location.longitude, radius).then((data) => {
-        setStations(data);
-      });
-    }
-  }
-
-  const callback = (region) => {
-    if(location) {
-      if(region.latitude == location.latitude && region.longitude == location.longitude) {
-        return;
-      }
-      setLocation(region);
-      loadStationData();
-    }
-  };
-
-
   return (
     <GestureHandlerRootView style={styles.container}>
       <MapView
         style={styles.mapView}
+        customMapStyle={mapStyle}
         showsUserLocation={true}
-        showsTraffic={true}
         zoomEnabled={false}
+        mapPadding={{top: 0, right: 0, bottom: 25, left: 0}}
         region={{
           latitude: location ? location.latitude : 51.165707,
           longitude: location ? location.longitude : 10.452764,
           latitudeDelta: (radius * 2) / 111.321,
           longitudeDelta: 0
         }}
-        onRegionChangeComplete={callback}
       >
+        <Circle
+          center={{
+            longitude: location ? location.longitude : 10.452764,
+            latitude: location ? location.latitude : 51.165707,
+          }}
+          radius={radius * 500}
+          fillColor="rgba(255, 0, 0, 0.1)"
+          strokeColor="rgba(255, 0, 0, 0.5)"
+          strokeWidth={1}
+        />
         {stations.map((marker, index) => (
           <Marker
             key={index}
@@ -64,6 +70,28 @@ function MapScreen() {
           />
         ))}
       </MapView>
+      <BottomSheet
+        snapPoints={[25, 140]}
+        initialSnapIndex={0}
+        activeOffsetY={[-1, 1]} 
+        failOffsetX={[-5, 5]}
+      >
+        <TextInput
+          style={{ margin: 10 }}
+          label="Adresse / Ort"
+          mode='outlined'
+          dense={true}
+          value={text}
+          onChangeText={text => setText(text)}
+        />
+        <Slider
+          style={{width: 200, height: 40}}
+          minimumValue={1}
+          maximumValue={10}
+          step={0.25}
+          onValueChange={(value) => setRadius(value)}
+        />
+      </BottomSheet>
     </GestureHandlerRootView>
   );
 }
@@ -75,7 +103,6 @@ const styles = StyleSheet.create({
   },
   mapView: {
     ...StyleSheet.absoluteFillObject,
-    marginBottom: 50,
   }
 });
 
