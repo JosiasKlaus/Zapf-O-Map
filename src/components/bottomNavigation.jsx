a86dbd94e7b91286b34687bbc235b4db39a0e462
Josias Klaus <josias.klaus@web.de>
2023-06-09 01:45:24 +0200

    Overhauled BottomNavigation 🚀

--- src/components/bottomNavigation.jsx
+++ src/components/bottomNavigation.jsx
@@ -1,56 +1,31 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
 import StationsList from '../views/listView.jsx';
-import MapScreen from "../views/mapView.jsx";
+import MapScreen from '../views/mapView.jsx';
+import {BottomNavigation} from 'react-native-paper';
+import React from 'react';
 
-function BottomNavigationComponent() {
-  const Tab = createMaterialBottomTabNavigator();
+const BottomNavigationComponent = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'list', title: 'Tankstellen', focusedIcon: 'gas-station'},
+    {key: 'map', title: 'Karte', focusedIcon: 'map-marker'},
+    {key: 'history', title: 'Tankbuch', focusedIcon: 'notebook'},
+    {key: 'settings', title: 'Einstellungen', focusedIcon: 'cog'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    list: StationsList,
+    map: MapScreen,
+    history: StationsList,
+    settings: StationsList,
+  });
 
   return (
-    <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen
-        name="Stations"
-        component={StationsList}
-        options={{
-          tabBarLabel: 'Tankstellen',
-          tabBarBadge: '1,799€',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="gas-station" color={color} size={26} />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="Map"
-        component={MapScreen}
-        options={{
-          tabBarLabel: 'Karte',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="map-marker" color={color} size={26} />
-          )
-        }}
-       />
-      {/* <Tab.Screen
-       name="History"
-       component={HistoryScreen}
-       options={{
-         tabBarLabel: 'Tankbuch',
-         tabBarIcon: ({ color }) => (
-           <MaterialCommunityIcons name="notebook" color={color} size={26} />
-         )
-       }}
-      />
-      <Tab.Screen
-       name="Settings"
-       component={HomeScreen}
-       options={{
-         tabBarLabel: 'Einstellungen',
-         tabBarIcon: ({ color }) => (
-           <MaterialCommunityIcons name="cog" color={color} size={26} />
-         )
-       }}
-      /> */}
-    </Tab.Navigator>
+    <BottomNavigation
+      shifting={true}
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   );
 }
 
