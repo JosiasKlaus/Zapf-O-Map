52f03b8a649c0213d68086e3d2df3fe9d8059cb3
Josias Klaus <josias.klaus@web.de>
2023-06-12 14:30:10 +0200

    Refactored components into seperate files 📄

--- src/components/stationSheet.jsx
+++ src/components/stationSheet.jsx
@@ -1,20 +1,17 @@
-import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
+import BottomSheet, {BottomSheetScrollView} from '@gorhom/bottom-sheet';
 import {StyleSheet} from 'react-native';
 import {Image, View} from 'react-native';
-import {Chip, DataTable, IconButton, Surface, Text} from 'react-native-paper';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import React, { useRef, useEffect, useState } from 'react';
-import { useTheme } from 'react-native-paper';
-import { getStationDetails } from '../api/tankerkoenig';
-import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
+import {IconButton, Text} from 'react-native-paper';
+import React, {useRef, useEffect, useState} from 'react';
+import {useTheme} from 'react-native-paper';
+import {getStationDetails} from '../api/tankerkoenig';
+import OpeningChip from './openingChip';
+import PricingTable from './pricingTable';
+import OpeningTable from './openingTable';
+import StationButtons from './stationButtons';
 
 // TODO: Get image from Maps API
 // TODO: Get station data from API (detailed info)
-
-const CHIP_ICON_CLOSED = 'close-circle';
-const CHIP_ICON_OPEN = 'check-circle';
-const CHIP_COLOR_CLOSED = '#E92C2C';
-const CHIP_COLOR_OPEN = '#00BA34';
 
 const StationSheet = ({initStation}) => {
   const bottomSheetRef = useRef(null);
@@ -24,7 +21,7 @@ const StationSheet = ({initStation}) => {
 
   useEffect(() => {
     bottomSheetRef.current.close();
-    if(initStation) {
+    if (initStation) {
       getStationDetails(initStation.id).then(data => {
         data.dist = initStation.dist;
         setStation(data);
@@ -66,75 +63,27 @@ const StationSheet = ({initStation}) => {
             </Text>
           </View>
           <View style={{marginLeft: 'auto'}}>
-            <Chip
-              style={{
-                backgroundColor: station?.isOpen ? CHIP_COLOR_OPEN + '1a' : CHIP_COLOR_CLOSED + '1a',
-                borderColor: station?.isOpen ? CHIP_COLOR_OPEN + '33' : CHIP_COLOR_CLOSED + '33',
-              }}
-              selectedColor={
-                station?.isOpen ? CHIP_COLOR_OPEN : CHIP_COLOR_CLOSED
-              }
-              mode="outlined"
-              icon={() => (
-                <Icon
-                  name={station?.isOpen ? CHIP_ICON_OPEN : CHIP_ICON_CLOSED}
-                  color={station?.isOpen ? CHIP_COLOR_OPEN : CHIP_COLOR_CLOSED}
-                  size={18}
-                />
-              )}
-            >
-              {station?.isOpen ? 'Geöffnet' : 'Geschlossen'}
-            </Chip>
+            <OpeningChip isOpen={station?.isOpen} />
             <Text style={styles.distance}>{station?.dist}km entfernt</Text>
           </View>
         </View>
-        <View style={{flexDirection: 'row'}}>
-          <IconButton icon="directions" size={24} mode="contained" />
-          <IconButton icon="phone" size={24} mode="contained" />
-          <IconButton icon="web" size={24} mode="contained" />
-        </View>
-        <View>
-          <Text style={[styles.title, {marginLeft: 10, marginTop: 10}]}>
-            Preise
-          </Text>
-          <DataTable style={styles.dataTable}>
-            <DataTable.Row>
-              <DataTable.Cell>E5</DataTable.Cell>
-              <DataTable.Cell numeric>{station?.e5}€</DataTable.Cell>
-            </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell>E10</DataTable.Cell>
-              <DataTable.Cell numeric>{station?.e10}€</DataTable.Cell>
-            </DataTable.Row>
-            <DataTable.Row>
-              <DataTable.Cell>Diesel</DataTable.Cell>
-              <DataTable.Cell numeric>{station?.diesel}€</DataTable.Cell>
-            </DataTable.Row>
-          </DataTable>
-        </View>
-        {station?.openingTimes ? (
-          <View>
-            <Text style={[styles.title, {marginLeft: 10, marginTop: 10}]}>
-              Öffnungszeiten
-            </Text>
-            <DataTable style={styles.dataTable}>
-              {station.openingTimes.map((openingTime, index) => (
-                <DataTable.Row key={index}>
-                  <DataTable.Cell>{openingTime.text}</DataTable.Cell>
-                  <DataTable.Cell numeric>
-                    {openingTime.start.slice(0, -3)} -{' '}
-                    {openingTime.end.slice(0, -3)} Uhr
-                  </DataTable.Cell>
-                </DataTable.Row>
-              ))}
-            </DataTable>
-            {station.overrides.length > 0 ? (
-              <Surface style={{borderRadius: 10}} mode="flat" elevation={2}>
-                <Text style={{margin: 10}}>{station.overrides.join(', ')}</Text>
-              </Surface>
-            ) : null}
-          </View>
-        ) : null}
+        <StationButtons stationId={station?.id} />
+        <PricingTable
+          prices={{
+            'Super E5': station?.e5,
+            'Super E10': station?.e10,
+            'Diesel': station?.diesel,
+          }}
+        />
+        {station?.wholeDay ? (
+          <Text style={{margin: 10}}>Diese Tankstelle ist rund um die Uhr geöffnet</Text>
+        ) : (
+          <OpeningTable
+            openingTimes={station?.openingTimes}
+            overrides={station?.overrides}
+            wholeDay={station?.wholeDay}
+          />
+        )}
       </BottomSheetScrollView>
     </BottomSheet>
   );
