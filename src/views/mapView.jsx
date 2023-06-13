91e40ebaeb8bb01678cc2ed371821ad7a58e5e9f
Josias Klaus <josias.klaus@web.de>
2023-06-14 00:11:16 +0200

    Added header to map view 🗺️

--- src/views/mapView.jsx
+++ src/views/mapView.jsx
@@ -1,16 +1,18 @@
-import {StyleSheet} from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 import {useState, useEffect, useRef} from 'react';
 import MapView, {Marker} from 'react-native-maps';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import Slider from '@react-native-community/slider';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
-import {useTheme} from 'react-native-paper';
+import {Appbar, Banner, useTheme} from 'react-native-paper';
 
 import {getLocation, calculateRegion} from '../utils/geolocation';
 import {getStationList} from '../api/tankerkoenig';
 import StationMarker from '../components/stationMarker';
 import StationSheet from '../components/stationSheet';
 import {mapStyleDark, mapStyleLight} from '../utils/mapstyle';
+import { BaseAnimationBuilder } from 'react-native-reanimated';
+import MapHeader from '../components/mapHeader';
 
 const DEFAULT_LATITUDE = 50.563527;
 const DEFAULT_LONGITUDE = 8.500261;
@@ -49,35 +51,38 @@ function MapScreen() {
   }, [location, radius]);
 
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <MapView
-        ref={mapRef}
-        style={styles.mapView}
-        customMapStyle={theme.dark ? mapStyleDark : mapStyleLight}
-        showsUserLocation={true}
-        zoomEnabled={false}
-        toolbarEnabled={false}
-        moveOnMarkerPress={false}
-        mapPadding={{top: insets.top, bottom: station ? 520 : 10, left: 20}}
-        region={calculateRegion(location?.latitude, location?.longitude, radius)}
-        onPress={() => setStation(null)}>
-        {stations.map((station, index) => (
-          <Marker
-            key={index}
-            tracksViewChanges={false}
-            coordinate={{longitude: station.lng, latitude: station.lat}}
-            onPress={() => {
-              setStation(station);
-              setTimeout(() => {
-                mapRef.current.animateToRegion(calculateRegion(station.lat, station.lng, radius), 100)
-              }, 100);
-            }}>
-            <StationMarker title={station.brand} price={station.e5} />
-          </Marker>
-        ))}
-      </MapView>
-      <StationSheet initStation={station} initSetStation={setStation} />
-    </GestureHandlerRootView>
+    <View style={styles.container}>
+      <MapHeader radius={radius} setRadius={setRadius}/>
+      <GestureHandlerRootView style={{flex: 1}}>
+        <MapView
+          ref={mapRef}
+          style={styles.mapView}
+          customMapStyle={theme.dark ? mapStyleDark : mapStyleLight}
+          showsUserLocation={true}
+          zoomEnabled={false}
+          toolbarEnabled={false}
+          moveOnMarkerPress={false}
+          mapPadding={{top: 10, bottom: station ? 520 : 10, left: 10, right: 10}}
+          region={calculateRegion(location?.latitude, location?.longitude, radius)}
+          onPress={() => setStation(null)}>
+          {stations.map((station, index) => (
+            <Marker
+              key={index}
+              tracksViewChanges={false}
+              coordinate={{longitude: station.lng, latitude: station.lat}}
+              onPress={() => {
+                setStation(station);
+                setTimeout(() => {
+                  mapRef.current.animateToRegion(calculateRegion(station.lat, station.lng, radius), 100)
+                }, 100);
+              }}>
+              <StationMarker title={station.brand} price={station.e5} />
+            </Marker>
+          ))}
+        </MapView>
+        <StationSheet initStation={station} initSetStation={setStation} />
+      </GestureHandlerRootView>
+    </View>
   );
 }
 
