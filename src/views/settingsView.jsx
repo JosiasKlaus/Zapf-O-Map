4bae73c4c9063869c34e3486c29275ce09f453c6
Josias Klaus <josias.klaus@web.de>
2023-06-14 12:57:58 +0200

    Added SettingsView ⚙️

---
+++ src/views/settingsView.jsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native-paper";
+import { View } from "react-native";
+
+const SettingsViewComponent = () => {
+  return (
+    <View>
+      <Text>Das hier sind die Einstellungen</Text>
+    </View>
+  );
+}
+
+export default SettingsViewComponent;
