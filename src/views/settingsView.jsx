336527d188bd61efc86241e3dcdd8c6905034b9f
Josias Klaus <josias.klaus@web.de>
2023-06-15 12:32:32 +0200

    Implimented fuel type dropdown ⛽

--- src/views/settingsView.jsx
+++ src/views/settingsView.jsx
@@ -1,10 +1,35 @@
-import { Text } from "react-native-paper";
+import { Appbar, Text } from "react-native-paper";
 import { View } from "react-native";
+import useAsyncStorage from "../hooks/useAsyncStorage";
+import DropDown from "react-native-paper-dropdown";
+import { useState } from "react";
 
 const SettingsViewComponent = () => {
+  const [fuelType, setFuelType] = useAsyncStorage("fuelType", "e5");
+  const [showDropDown, setShowDropDown] = useState(false);
+  const fuelTypeList = [
+    { label: "Super E5", value: "e5" },
+    { label: "Super E10", value: "e10" },
+    { label: "Diesel", value: "diesel" },
+  ];
+
   return (
     <View>
-      <Text>Das hier sind die Einstellungen</Text>
+      <Appbar.Header>
+        <Appbar.Content title="Einstellungen" />
+      </Appbar.Header>
+      <View style={{ marginHorizontal: 20 }}>
+        <DropDown
+          label={"Standard Kraftstoff"}
+          mode={"outlined"}
+          visible={showDropDown}
+          showDropDown={() => setShowDropDown(true)}
+          onDismiss={() => setShowDropDown(false)}
+          value={fuelType}
+          setValue={setFuelType}
+          list={fuelTypeList}
+        />
+      </View>
     </View>
   );
 }
